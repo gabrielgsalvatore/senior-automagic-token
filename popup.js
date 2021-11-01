a46b5d6369069ee0c991b457a73fc6ed6db8dc7c
gabrielgsalvatore <gabrielgarciasalvador@outlook.com>
2021-11-01 15:39:16 -0300

    Adicionado user e tenant, ajustes css

--- popup.js
+++ popup.js
@@ -1,60 +1,73 @@
-
 function searchBearerToken() {
-  chrome.cookies.get({
-    url: "https://www.platform.senior.com.br",
-    name: "com.senior.pau.token"
-  }, function(cookie) {
-    if (cookie) {
-      var parsedCookie = JSON.parse(JSON.parse(cookie.value));
-      const bearerToken = "Bearer "+parsedCookie.access_token;
-      console.log(bearerToken);
-      document.getElementById("text").innerHTML = bearerToken;
+    chrome.cookies.get({
+        url: "https://www.platform.senior.com.br",
+        name: "com.senior.pau.token"
+    }, function(cookie) {
+        if (cookie) {
+            var parsedCookie = JSON.parse(JSON.parse(cookie.value));
+            const bearerToken = "Bearer " + parsedCookie.access_token;
+            document.getElementById("text").innerHTML = bearerToken;
 
-    } else {
-      document.getElementById("text").innerHTML = "&Eacute; necess&aacute;rio estar autenticado na plataforma G7";
-    }
-  });
+        } else {
+            document.getElementById("text").innerHTML = "&Eacute; necess&aacute;rio estar autenticado na plataforma G7";
+        }
+    });
+}
+
+function searchUserAndTenant() {
+    chrome.cookies.get({
+        url: "https://www.platform.senior.com.br",
+        name: "com.senior.pau.token"
+    }, function(cookie) {
+        if (cookie) {
+            var parsedCookie = JSON.parse(JSON.parse(cookie.value));
+            const user = parsedCookie.username.split('@')[0];
+            const tenant = parsedCookie.username.split('@')[1];
+            document.getElementById("tenant").innerHTML = tenant;
+            document.getElementById("user").innerHTML = user;
+        }
+    });
 }
 
 function searchToken() {
-  chrome.cookies.get({
-    url: "https://www.platform.senior.com.br",
-    name: "com.senior.pau.token"
-  }, function(cookie) {
-    if (cookie) {
-      var parsedCookie = JSON.parse(JSON.parse(cookie.value));
-      const bearerToken = parsedCookie.access_token;
-      console.log(bearerToken);
-      document.getElementById("text").innerHTML = bearerToken;
+    chrome.cookies.get({
+        url: "https://www.platform.senior.com.br",
+        name: "com.senior.pau.token"
+    }, function(cookie) {
+        if (cookie) {
+            var parsedCookie = JSON.parse(JSON.parse(cookie.value));
+            const bearerToken = parsedCookie.access_token;
+            document.getElementById("text").innerHTML = bearerToken;
 
-    } else {
-      document.getElementById("text").innerHTML = "&Eacute; necess&aacute;rio estar autenticado na plataforma G7";
-    }
-  });
+        } else {
+            document.getElementById("text").innerHTML = "&Eacute; necess&aacute;rio estar autenticado na plataforma G7";
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("copybtn").addEventListener("click", copyText);
-  const checkBearer = document.getElementById("bearer_check");
-  checkBearer.addEventListener("change", function(){
-    if(checkBearer.checked){
-      searchToken();
-    }else{
-      searchBearerToken();
-    }
-  });
+    document.getElementById("copybtn").addEventListener("click", copyText);
+    const checkBearer = document.getElementById("bearer_check");
+    checkBearer.addEventListener("change", function() {
+        if (checkBearer.checked) {
+            searchToken();
+        } else {
+            searchBearerToken();
+        }
+    });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("copybtn").addEventListener("click", copyText);
+    document.getElementById("copybtn").addEventListener("click", copyText);
 });
 
-function copyText(){
-  var copyText = document.getElementById("text");
-  copyText.select();
-  navigator.clipboard.writeText(copyText.value);
+function copyText() {
+    var copyText = document.getElementById("text");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
 }
 
 window.onload = function() {
-  searchBearerToken();
+    searchBearerToken();
+    searchUserAndTenant();
 }
